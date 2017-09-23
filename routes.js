c4291aed96afe1430baa1437f6a6b847207a0fbc
Daniel Blackburn <craklyn@gmail.com>
2017-09-23 15:49:37 -0700

    Exposing static directory for images and css

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 module.exports = function(app){
-    var api = require('./controllers/api');
-    app.get('/index', api.getHome);   
-    app.get('/style.css', api.getStyleSheet);   
+  var express = require('express');
+  app.use(express.static(__dirname + '/public/stylesheets'));
+
+  var api = require('./controllers/api');
+  app.get('/index', api.getHome);   
+  app.get('/style.css', api.getStyleSheet);     
+  app.use(express.static('public'))
 }
