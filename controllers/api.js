f02342e95204e48d7a7b5f2d23d47cb22805d787
Daniel Blackburn <craklyn@gmail.com>
2017-09-23 13:19:41 -0700

    Separated css from html

--- controllers/api.js
+++ controllers/api.js
@@ -3,3 +3,8 @@ fs = require('fs');
 exports.getHome = function(req, res){
   fs.createReadStream("controllers/index.html").pipe(res);
 };
+
+exports.getStyleSheet = function(req, res){
+  fs.createReadStream("public/stylesheets/style.css").pipe(res);
+};
+
--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 module.exports = function(app){
     var api = require('./controllers/api');
     app.get('/index', api.getHome);   
+    app.get('/style.css', api.getStyleSheet);   
 }
