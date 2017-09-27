10f43b5b5d2647d2ceeadeb45c880f8f9d73ec80
Daniel Blackburn <craklyn@gmail.com>
2017-09-27 10:50:13 -0700

    Trying small changes to successfully launch on heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path = require('path');
 require('./routes')(app);
 
 // Set server port
-var port = process.env.npm_package_config_port;
+console.log(process)
+var port = process.env.PORT || process.env.npm_package_config_port || 8080;
 app.listen(port);
 console.log('Server running at http://localhost:' + port + '/');
