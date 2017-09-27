7a67d13aca5e026e4ea89475c12291dc05789324
Daniel Blackburn <craklyn@gmail.com>
2017-09-27 11:17:30 -0700

    Trying small changes to successfully launch on heroku

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 module.exports = function(app){
   var express = require('express');
   app.use(express.static(__dirname + '/public/stylesheets'));
-  app.use(express.static(__dirname + '/public/images'));
+  //app.use(express.static(__dirname + '/public/images'));
+  var path = require('path');
+  app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
   var api = require('./controllers/api');
   app.get('/index', api.getHome);   
